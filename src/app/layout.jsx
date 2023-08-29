0447eb3f072bc91cde64bf6e1b0ffd702380447e
Stav Tohami <tohami2411@gmail.com>
2023-08-29 17:06:27 +0300

    add footer cmp

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,14 @@
-import './globals.css'
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* FOOTER */}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function CountryLookup() {
+  const [country, setCountry] = useState("United States");
+
+  useEffect(() => {
+    fetchCountryUser();
+  }, []);
+
+  const fetchCountryUser = async () => {
+    try {
+      const res = await axios.get(
+        `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_API_KEY}`
+      );
+      console.log(res);
+      if (!res) return;
+      setCountry(res.data.country);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+  <div>{country}</div>
+  )
+}
