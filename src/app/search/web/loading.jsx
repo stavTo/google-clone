d563a211f7f7e0ab69ae2077c192be5b681fdaee
Stav Tohami <tohami2411@gmail.com>
2023-08-30 14:26:36 +0300

    loading effects

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,30 @@
+export default function loading() {
+  return (
+    <>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 h-2.5 max-w-48 rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-3.5 max-w-[360px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[560px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[530px] rounded-full mb-2.5"></div>
+      </div>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 h-2.5 max-w-48 rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-3.5 max-w-[360px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[560px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[530px] rounded-full mb-2.5"></div>
+      </div>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 h-2.5 max-w-48 rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-3.5 max-w-[360px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[560px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[530px] rounded-full mb-2.5"></div>
+      </div>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 h-2.5 max-w-48 rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-3.5 max-w-[360px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[560px] rounded-full mb-2.5"></div>
+        <div className="bg-gray-200 h-2 max-w-[530px] rounded-full mb-2.5"></div>
+      </div>
+    </>
+  );
+}
