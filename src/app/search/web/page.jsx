ba34ed63592b051ebf2b2431afa716cdd20fc26e
Stav Tohami <tohami2411@gmail.com>
2023-08-30 11:15:16 +0300

    fetch data from google search api and show the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,11 @@
+import axios from "axios";
+import React from "react";
 
-import React from 'react'
-
-export default function WebSearchPage() {
-  return (
-    <div>WebSearchPage</div>
-  )
+export default async function WebSearchPage({ searchParams }) {
+  const response =
+    await axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+  `);
+  const results = response.data.items;
+  console.log(results);
+  return <>{results && results.map((res) => <h1>{res.title}</h1>)}</>;
 }
